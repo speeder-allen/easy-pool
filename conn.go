a41e5487f02498e241ecbdfc614d62dd067d7bf1
Barry Allen <future.speeder@gmail.com>
2019-09-09 23:40:31 +0800

    add time uuid to conn

--- conn.go
+++ conn.go
@@ -1,9 +1,15 @@
 package easy_pool
 
-import "io"
+import (
+	"io"
+	"time"
+)
 
 type Conn struct {
-	Raw interface{}
+	UUID string
+	Raw  interface{}
+	nsec uint32
+	sec  int64
 }
 
 func (c *Conn) Close() error {
@@ -11,7 +17,10 @@ func (c *Conn) Close() error {
 		if r, ok := c.Raw.(io.Closer); ok {
 			return r.Close()
 		}
-
 	}
 	return nil
+}
+
+func (c *Conn) Time() time.Time {
+	return time.Unix(c.sec, int64(c.nsec))
 }
