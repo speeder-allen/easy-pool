4678f362248cd67e669e3817e3dc250e754a33e6
Barry Allen <future.speeder@gmail.com>
2019-09-09 23:46:56 +0800

    modifiy conn_test

--- conn_test.go
+++ conn_test.go
@@ -1,26 +1,36 @@
 package easy_pool_test
 
 import (
-	"fmt"
+	"errors"
 	easy_pool "github.com/speeder-allen/easy-pool"
 	"gotest.tools/assert"
+	"sync/atomic"
 	"testing"
 )
 
-type testconn struct {
-	Closed bool
+type mockconnection struct {
+	closed uint32
 }
 
-func (t *testconn) Close() error {
-	fmt.Println("closed.")
-	t.Closed = true
-	return nil
+var ErrorMockClose = errors.New("just a mock")
+
+func (m *mockconnection) Close() error {
+	atomic.CompareAndSwapUint32(&m.closed, 0, 1)
+	return ErrorMockClose
+}
+
+type mockconn struct {
 }
 
 func TestConn_Close(t *testing.T) {
-	tt := testconn{}
-	c := easy_pool.Conn{&tt}
-	assert.Equal(t, tt.Closed, false)
-	c.Close()
-	assert.Equal(t, tt.Closed, true)
+	mock := &mockconnection{closed: 0}
+	c := easy_pool.Conn{Raw: mock}
+	assert.Equal(t, mock.closed, uint32(0))
+	err := c.Close()
+	assert.Equal(t, err, ErrorMockClose)
+	assert.Equal(t, mock.closed, uint32(1))
+	m := mockconn{}
+	cc := easy_pool.Conn{Raw: &m}
+	err = cc.Close()
+	assert.NilError(t, err)
 }
